62da3c8efff31441948023d88aad77381e2d1652
hocine1984 <damir.dam84@gmail.com>
2025-07-29 22:32:35 +0100

    Add_Bottom FieldSheet

--- lib/views/Widjets/Custom_TexteField.dart
+++ lib/views/Widjets/Custom_TexteField.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 
 class CustomTextefield extends StatelessWidget {
-  const CustomTextefield({super.key});
+  const CustomTextefield({super.key, required this.hint, this.MaxLine = 1});
+  final String hint;
+  final int MaxLine;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: MaxLine,
       decoration: InputDecoration(
-        hintText: "Title",
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(16),
-          borderSide: BorderSide(color: Colors.red),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(24),
-          borderSide: BorderSide(color: Colors.green),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(24),
-          borderSide: BorderSide(color: Colors.yellow),
-        ),
+        hintText: hint,
+        border: outLineInputBorder(),
+        enabledBorder: outLineInputBorder(),
+        focusedBorder: outLineInputBorder(Colors.blue),
       ),
+    );
+  }
+
+  OutlineInputBorder outLineInputBorder([Color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: Color ?? Colors.white),
     );
   }
 }
--- lib/views/Widjets/add_New_Note_BottomSheet.dart
+++ lib/views/Widjets/add_New_Note_BottomSheet.dart
@@ -8,7 +8,14 @@ class AddNewNoteBottomsheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(children: [SizedBox(height: 24), CustomTextefield()]),
+      child: Column(
+        children: [
+          SizedBox(height: 24),
+          CustomTextefield(hint: 'Title'),
+          SizedBox(height: 16),
+          CustomTextefield(hint: "Content", MaxLine: 5),
+        ],
+      ),
     );
   }
 }
